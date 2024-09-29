f9ba8850fb272bc89eb00612bc4e789938d5b9d3
Adelina <adisulda123@gmail.com>
2024-09-29 14:03:28 +0500

    Новые функции

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+const lineLength = function(line, linelength){
+  return line.length <= linelength;
+};
+
+lineLength('проверяемая строка', 25);
+lineLength('sdss',3);
+
+const palindrom = function(line){
+  const normalLine = (line.replaceAll(' ','')).toLowerCase();
+  let clearLine = '';
+  for(let i = normalLine.length - 1;i >= 0;i--){
+    clearLine += normalLine[i];
+  }
+  return normalLine === clearLine;
+};
+
+palindrom('топот');
+
