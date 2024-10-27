efe1e5dbe7c60961f2875eedd343aaad615bfebc
Adelina <adisulda123@gmail.com>
2024-10-27 12:56:36 +0500

    Делу-время

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,19 @@ const palindrom = function(line){
 
 palindrom('топот');
 
+const isWorkMeeting = function(startWorking, endWorking, startMeeting, meetingTime){
+  const timeToMinutes = function(time){
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+
+  const startMeetingInMinutes = timeToMinutes(startMeeting);
+  const startWorkingDayInMinutes = timeToMinutes(startWorking);
+  const endWorkingDayInMinutes = timeToMinutes(endWorking);
+  const endMeetingInMinutes = startMeetingInMinutes + meetingTime;
+
+  return startMeetingInMinutes >= startWorkingDayInMinutes && endMeetingInMinutes <= endWorkingDayInMinutes;
+};
+
+
+isWorkMeeting('14:00', '17:30', '08:0', 90);
