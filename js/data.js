2a65b7542a986901b6217ed970f5b846f05c6617
Adelina <adisulda123@gmail.com>
2024-12-25 23:40:10 +0500

    откатила коммиты

--- js/data.js
+++ js/data.js
@@ -1,7 +1,47 @@
-import {getData} from './api.js';
-const url = 'https://29.javascript.htmlacademy.pro/kekstagram/data';
-const generatePhotos = async()=>{
-  const photos = await getData(url);
+import { getRandomInt } from './util.js';
+import { getRandomArray } from './util.js';
+const generateComment = function (count) {
+  const messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const comments = [];
+  const names = ['Борис', 'Мурка', 'Пушок', 'Феликс', 'Котофей', 'Кекс', 'Карамелька', 'Мурзик', 'Барсик', 'Дымок'];
+
+  for (let i = 1; i < count; i++) {
+    comments.push({
+      id: i,
+      avatar: `img/avatar-${getRandomInt(1, 7)}.svg`,
+      message: getRandomArray(messages),
+      name: getRandomArray(names)
+    });
+  }
+  return comments;
+};
+
+const generatePhotos = () => {
+  const photos = [];
+  const descriptions = [
+    'Уютненько',
+    'Красивенько',
+    'Красота',
+    'Хорошего Вам дня!',
+    'Незабываемый день!'
+  ];
+  for (let i = 1; i <= 25; i++) {
+    const photo = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomArray(descriptions),
+      likes: getRandomInt(15, 200),
+      comments: generateComment(getRandomInt(0, 30)),
+    };
+    photos.push(photo);
+  }
   return photos;
 };
 
--- js/util.js
+++ js/util.js
@@ -3,22 +3,4 @@ export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 export const getRandomArray = (elements) => elements[getRandomInt(0, elements.length - 1)];
-
-const showError = () => {
-  const error = document.createElement('div');
-  error.style.color = 'red';
-  error.style.position = 'fixed';
-  error.style.fontWeight = 'bold';
-  error.style.top = '0';
-  error.style.left = '0';
-  error.style.width = '100%';
-  error.style.backgroundColor = 'white';
-  error.style.borderBottom = '2px solid red';
-  error.style.zIndex = '1000';
-  error.innerText = 'Упс, ошибка со стороны сервера, попробуйте перезагрузить страницу или зайти попозже :(';
-  error.style.textAlign = 'center';
-  document.body.appendChild(error);
-};
-
-
-export default {isEscapeKey,showError};
+export default {isEscapeKey};
